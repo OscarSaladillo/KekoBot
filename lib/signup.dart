cbfce37ecd0abac35f2f1896cb179a435a9fd8c6
AHaizaki <andoitz.haizaki@gmail.com>
2022-05-25 21:42:13 +0200

    Segunda pantalla

--- lib/signup.dart
+++ lib/signup.dart
@@ -1,7 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:email_validator/email_validator.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Register extends StatefulWidget {
@@ -17,10 +16,8 @@ class _RegisterState extends State<Register> {
   bool emailInUse = false;
   TextEditingController emailCtrl = TextEditingController();
   TextEditingController confirmEmailCtrl = TextEditingController();
-  TextEditingController nameCtrl = TextEditingController();
-  TextEditingController lastNameCtrl = TextEditingController();
+  TextEditingController usernameCtrl = TextEditingController();
   TextEditingController passwordCtrl = TextEditingController();
-  TextEditingController nacionalityCtrl = TextEditingController();
   TextEditingController confirmPasswordCtrl = TextEditingController();
   String emailError = "Debe ser un email valido";
 
@@ -37,13 +34,9 @@ class _RegisterState extends State<Register> {
         FirebaseAuth auth = FirebaseAuth.instance;
         await auth.createUserWithEmailAndPassword(
             email: email, password: password);
-        FirebaseFirestore.instance.collection("users").add({
-          "color": "0xFFFFFFFF",
-          "email": email,
-          "lastnames": lastNameCtrl.text,
-          "nacionality": nacionalityCtrl.text,
-          "name": nameCtrl.text
-        });
+        FirebaseFirestore.instance
+            .collection("users")
+            .add({"avatar": "", "email": email, "username": usernameCtrl.text});
         return true;
       } else {
         return false;
@@ -62,180 +55,151 @@ class _RegisterState extends State<Register> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: AppBar(
+          title: const Text("Registrarse"),
+        ),
         body: SingleChildScrollView(
-      child: Container(
-        margin: const EdgeInsets.symmetric(vertical: 20),
-        height: 700,
-        child: Form(
-            key: _formKey,
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Container(
-                      margin: const EdgeInsets.symmetric(horizontal: 20),
-                      child: TextFormField(
-                          controller: nameCtrl,
-                          validator: (name) {
-                            if (name!.isEmpty) {
-                              return 'El nombre no debe estar vacio';
-                            }
-                            return null;
-                          },
-                          decoration: const InputDecoration(
-                            border: OutlineInputBorder(
-                                borderSide:
-                                    BorderSide(color: Colors.black, width: 5)),
-                            labelText: 'Nombre',
-                          ))),
-                  Container(
-                      margin: const EdgeInsets.symmetric(horizontal: 20),
-                      child: TextFormField(
-                          controller: lastNameCtrl,
-                          validator: (lastName) {
-                            if (lastName!.isEmpty) {
-                              return 'El apellido no debe estar vacio';
-                            }
-                            return null;
-                          },
-                          decoration: const InputDecoration(
-                            border: OutlineInputBorder(
-                                borderSide:
-                                    BorderSide(color: Colors.black, width: 5)),
-                            labelText: 'Apellidos',
-                          ))),
-                  Container(
-                      margin: const EdgeInsets.symmetric(horizontal: 20),
-                      child: TextFormField(
-                          controller: nacionalityCtrl,
-                          validator: (nacionality) {
-                            if (nacionality!.isEmpty) {
-                              return 'La nacionalidad no debe estar vacia';
-                            }
-                            return null;
-                          },
-                          decoration: const InputDecoration(
-                            border: OutlineInputBorder(
-                                borderSide:
-                                    BorderSide(color: Colors.black, width: 5)),
-                            labelText: 'Nacionalidad',
-                          ))),
-                  Container(
-                      margin: const EdgeInsets.symmetric(horizontal: 20),
-                      child: TextFormField(
-                          controller: emailCtrl,
-                          validator: (email) {
-                            if (!EmailValidator.validate(email!) ||
-                                emailInUse) {
-                              return emailError;
-                            }
-                            return null;
-                          },
-                          decoration: const InputDecoration(
-                            border: OutlineInputBorder(
-                                borderSide:
-                                    BorderSide(color: Colors.black, width: 5)),
-                            labelText: 'Email',
-                          ))),
-                  Container(
-                      margin: const EdgeInsets.symmetric(horizontal: 20),
-                      child: TextFormField(
-                          controller: confirmEmailCtrl,
-                          validator: (email) {
-                            if (email != emailCtrl.text) {
-                              return 'los email no son iguales';
-                            }
-                            return null;
-                          },
-                          decoration: const InputDecoration(
-                            border: OutlineInputBorder(
-                                borderSide:
-                                    BorderSide(color: Colors.black, width: 5)),
-                            labelText: 'Confirmar Email',
-                          ))),
-                  Container(
-                    margin: const EdgeInsets.symmetric(horizontal: 20),
-                    child: TextFormField(
-                        controller: passwordCtrl,
-                        validator: (password) {
-                          if (password!.length > 10) {
-                            return 'Máximo 10 caracteres';
-                          } else if (password.length < 3) {
-                            return 'Mínimo 3 caracteres';
-                          }
-                          return null;
-                        },
-                        obscureText: true,
-                        enableSuggestions: false,
-                        autocorrect: false,
-                        decoration: const InputDecoration(
-                          border: OutlineInputBorder(
-                              borderSide:
-                                  BorderSide(color: Colors.black, width: 5)),
-                          labelText: 'Contraseña',
-                        )),
-                  ),
-                  Container(
-                    margin: const EdgeInsets.symmetric(horizontal: 20),
-                    child: TextFormField(
-                        controller: confirmPasswordCtrl,
-                        validator: (password) {
-                          if (password != passwordCtrl.text) {
-                            return 'Debe ser la misma contraseña';
-                          }
-                          return null;
-                        },
-                        obscureText: true,
-                        enableSuggestions: false,
-                        autocorrect: false,
-                        decoration: const InputDecoration(
-                          border: OutlineInputBorder(
-                              borderSide:
-                                  BorderSide(color: Colors.black, width: 5)),
-                          labelText: 'Confirmar contraseña',
-                        )),
-                  ),
-                  Container(
-                    child: ElevatedButton(
-                        onPressed: () async {
-                          if (emailError != "El email debe ser valido") {
-                            setState(() {
-                              emailError = "El email debe ser valido";
-                            });
-                          }
-                          allowEnter = await checkEmail(
-                              emailCtrl.text, passwordCtrl.text);
-                          //Esta condicion parece redundante con la condicion
-                          //del checkEmail,pero sirve
-                          //para comprobar si el correo esta en uso o no
-                          //porque el anterior comprueba si el correo es valido
-                          if (_formKey.currentState!.validate()) {
-                            FocusManager.instance.primaryFocus?.unfocus();
-                            ScaffoldMessenger.of(context).showSnackBar(
-                                const SnackBar(
-                                    content: Text(
-                                        "Se ha efectuado el registro con exito")));
-                            Navigator.pop(context);
-                          }
-                        },
-                        child: const Text('Registrar'),
-                        style: ButtonStyle(
-                            padding: MaterialStateProperty.all<EdgeInsets>(
-                                const EdgeInsets.symmetric(
-                                    vertical: 20, horizontal: 50)),
-                            backgroundColor:
-                                MaterialStateProperty.all(Colors.orange[200]),
-                            shape: MaterialStateProperty.all(
-                                RoundedRectangleBorder(
-                                    side: const BorderSide(
-                                        color: Color(0xFFE77A06),
-                                        width: 1,
-                                        style: BorderStyle.solid),
-                                    borderRadius:
-                                        BorderRadius.circular(30.0))))),
-                    margin: const EdgeInsets.symmetric(vertical: 10),
-                  )
-                ])),
-      ),
-    ));
+          child: Container(
+            margin: const EdgeInsets.symmetric(vertical: 20),
+            height: 700,
+            child: Form(
+                key: _formKey,
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Container(
+                          margin: const EdgeInsets.symmetric(horizontal: 20),
+                          child: TextFormField(
+                              controller: usernameCtrl,
+                              validator: (name) {
+                                if (name!.isEmpty) {
+                                  return 'El nickname no debe estar vacio';
+                                }
+                                return null;
+                              },
+                              decoration: const InputDecoration(
+                                border: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                        color: Colors.black, width: 5)),
+                                labelText: 'Nickname',
+                              ))),
+                      Container(
+                          margin: const EdgeInsets.symmetric(horizontal: 20),
+                          child: TextFormField(
+                              controller: emailCtrl,
+                              validator: (email) {
+                                if (!EmailValidator.validate(email!) ||
+                                    emailInUse) {
+                                  return emailError;
+                                }
+                                return null;
+                              },
+                              decoration: const InputDecoration(
+                                border: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                        color: Colors.black, width: 5)),
+                                labelText: 'Email',
+                              ))),
+                      Container(
+                          margin: const EdgeInsets.symmetric(horizontal: 20),
+                          child: TextFormField(
+                              controller: confirmEmailCtrl,
+                              validator: (email) {
+                                if (email != emailCtrl.text) {
+                                  return 'los email no son iguales';
+                                }
+                                return null;
+                              },
+                              decoration: const InputDecoration(
+                                border: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                        color: Colors.black, width: 5)),
+                                labelText: 'Confirmar Email',
+                              ))),
+                      Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 20),
+                        child: TextFormField(
+                            controller: passwordCtrl,
+                            validator: (password) {
+                              if (password!.length > 10) {
+                                return 'Máximo 10 caracteres';
+                              } else if (password.length < 3) {
+                                return 'Mínimo 3 caracteres';
+                              }
+                              return null;
+                            },
+                            obscureText: true,
+                            enableSuggestions: false,
+                            autocorrect: false,
+                            decoration: const InputDecoration(
+                              border: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                      color: Colors.black, width: 5)),
+                              labelText: 'Contraseña',
+                            )),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 20),
+                        child: TextFormField(
+                            controller: confirmPasswordCtrl,
+                            validator: (password) {
+                              if (password != passwordCtrl.text) {
+                                return 'Debe ser la misma contraseña';
+                              }
+                              return null;
+                            },
+                            obscureText: true,
+                            enableSuggestions: false,
+                            autocorrect: false,
+                            decoration: const InputDecoration(
+                              border: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                      color: Colors.black, width: 5)),
+                              labelText: 'Confirmar contraseña',
+                            )),
+                      ),
+                      Container(
+                        child: ElevatedButton(
+                            onPressed: () async {
+                              if (emailError != "El email debe ser valido") {
+                                setState(() {
+                                  emailError = "El email debe ser valido";
+                                });
+                              }
+                              allowEnter = await checkEmail(
+                                  emailCtrl.text, passwordCtrl.text);
+                              //Esta condicion parece redundante con la condicion
+                              //del checkEmail,pero sirve
+                              //para comprobar si el correo esta en uso o no
+                              //porque el anterior comprueba si el correo es valido
+                              if (_formKey.currentState!.validate()) {
+                                FocusManager.instance.primaryFocus?.unfocus();
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                    const SnackBar(
+                                        content: Text(
+                                            "Se ha efectuado el registro con exito")));
+                                Navigator.pop(context);
+                              }
+                            },
+                            child: const Text('Registrar'),
+                            style: ButtonStyle(
+                                padding: MaterialStateProperty.all<EdgeInsets>(
+                                    const EdgeInsets.symmetric(
+                                        vertical: 20, horizontal: 50)),
+                                backgroundColor: MaterialStateProperty.all(
+                                    Colors.orange[200]),
+                                shape: MaterialStateProperty.all(
+                                    RoundedRectangleBorder(
+                                        side: const BorderSide(
+                                            color: Color(0xFFE77A06),
+                                            width: 1,
+                                            style: BorderStyle.solid),
+                                        borderRadius:
+                                            BorderRadius.circular(30.0))))),
+                        margin: const EdgeInsets.symmetric(vertical: 10),
+                      )
+                    ])),
+          ),
+        ));
   }
 }
